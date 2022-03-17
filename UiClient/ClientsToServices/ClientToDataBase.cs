7be1348932cd324bbda3abf0607380e4896714c7
Георгий Рябов <g.riabov@ia-group.ru>
2022-03-17 16:00:09 +0300

    added client to UIClient

---
+++ UiClient/ClientsToServices/ClientToDataBase.cs
@@ -0,0 +1,33 @@
+﻿using Grpc.Net.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UiClient.ClientsToServices
+{
+    public class ClientToDataBaseService
+    {
+        //TODO: Change this client to TGBot
+        public ClientToDataBaseService()
+        {
+
+        }
+        public void SendMessage(string name)
+        {
+
+            // создаем канал для обмена сообщениями с сервером
+            // параметр - адрес сервера gRPC
+            using var channel = GrpcChannel.ForAddress("http://localhost:4999");
+            // создаем клиента
+            var client = new DataBaseService.DataBase.DataBaseClient(channel);
+
+            //Console.Write("Введите имя: ");
+            //string name = Console.ReadLine();
+            // обмениваемся сообщениями с сервером
+            var ryply = client.SayHello(new DataBaseService.HelloRequest { Name = name });
+            //Console.WriteLine(ryply);
+            //var reply = await client.SayHelloAsync(new HelloRequest { Name = name });
+        }
+    }
+}
---
+++ UiClient/ClientsToServices/ClientToNotify.cs
@@ -0,0 +1,32 @@
+﻿using Grpc.Net.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UiClient.ClientsToServices
+{
+    public class ClientToNotify
+    {
+        public ClientToNotify()
+        {
+
+        }
+        public void SendMessage(string name)
+        {
+
+            // создаем канал для обмена сообщениями с сервером
+            // параметр - адрес сервера gRPC
+            using var channel = GrpcChannel.ForAddress("http://localhost:4999");
+            // создаем клиента
+            var client = new MessageNotifyerService.MessageNotifyer.MessageNotifyerClient(channel);
+
+            //Console.Write("Введите имя: ");
+            //string name = Console.ReadLine();
+            // обмениваемся сообщениями с сервером
+            var ryply = client.SayHello(new MessageNotifyerService.HelloRequest { Name = name });
+            //Console.WriteLine(ryply);
+            //var reply = await client.SayHelloAsync(new HelloRequest { Name = name });
+        }
+    }
+}
